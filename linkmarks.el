73637ecf493f01070cb3c0186e14c93c917f4f74
Dustin Lacewell <dlacewell@gmail.com>
2018-08-30 04:13:00 -0500

    initial commit

---
+++ linkmarks.el
@@ -0,0 +1,76 @@
+;;; -*- lexical-binding: t -*-
+;;; linkmarks.el --- Org-mode link based bookmarks
+;; Copyright (C) 2018 Dustin Lacewell
+
+;; Author: Dustin Lacewell <dlacewell@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "24") (helm "0") (dash "0")
+;; Keywords: hydra
+;; URL: http://github.com/dustinlacewell/hera
+
+;;; Commentary:
+
+;; This package lets you use org-mode links as bookmarks.
+
+;;; Code:
+(require 'seq)
+(require 'helm)
+(require 'dash)
+
+(setq linkmarks-file (expand-file-name "~/org/bookmarks.org"))
+
+(defun linkmarks--setup ()
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-link-frame-setup '((elisp . find-file)
+                               (vm . vm-visit-folder-other-frame)
+                               (vm-imap . vm-visit-imap-folder-other-frame)
+                               (gnus . org-gnus-no-new-news)
+                               (file . find-file)
+                               (wl . wl-other-frame)))
+  (setq org-refile-use-outline-path t)
+  (setq org-refile-targets `((,linkmarks-file :maxlevel . 99)))
+  (setq org-confirm-elisp-link-function nil)
+  (setq safe-local-variable-values '((org-confirm-elisp-link-function . nil))))
+
+(defun linkmarks--in-file ()
+  (linkmarks--setup)
+  (with-temp-buffer
+    (insert-file-contents linkmarks-file t)
+    (org-mode)
+    (beginning-of-buffer)
+    (outline-show-all)
+    (cl-loop
+     for target in (org-refile-get-targets)
+     for element = (progn
+                     (goto-char (nth 3 target))
+                     (next-line)
+                     (org-element-context))
+     for type = (car element)
+     for props = (cadr element)
+     for begin = (plist-get props :begin)
+     for end = (plist-get props :end)
+     for content = (buffer-substring begin end)
+     if (equal 'link type)
+     collect (list (car target) content))))
+
+(defun linkmarks-select ()
+  (interactive)
+  (-let* ((targets (linkmarks--in-file))
+          (choices (mapcar 'car targets))
+          (choice (completing-read "Bookmark" choices))
+          ((_ link) (-first (lambda (i) (equal (car i) choice)) targets)))
+    (org-open-link-from-string link)))
+
+(defun linkmarks-capture ()
+  (interactive)
+  (let ((org-capture-entry '("t" "Bookmark" entry (file "~/org/bookmarks.org")
+                             "* %^{Title}\n[[%?]]\n  added: %U" '(:kill-buffer))))
+    (linkmarks--setup)
+    (org-capture)))
+
+(nougat-hydra hydra-bookmarks (:color blue)
+  ("Bookmarks" (("n" (linkmarks-capture) "new")
+                ("b" (linkmarks-select) "browse"))))
+
+(provide 'linkmarks)
+;;; linkmarks.el ends here
