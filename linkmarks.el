87b4a304a1fcca3a52e85cd42765702b979bfb2e
Dustin Lacewell <dlacewell@gmail.com>
2018-09-03 09:59:27 -0500

    Fixed up some things thanks to github.com/etu

--- linkmarks.el
+++ linkmarks.el
@@ -1,25 +1,34 @@
-;;; -*- lexical-binding: t -*-
-;;; linkmarks.el --- Org-mode link based bookmarks
+;;; linkmarks.el --- Org-mode link based bookmarks -*- lexical-binding: t; -*-
 ;; Copyright (C) 2018 Dustin Lacewell
 
 ;; Author: Dustin Lacewell <dlacewell@gmail.com>
-;; Version: 0.1
-;; Package-Requires: ((emacs "24") (helm "0") (dash "0")
-;; Keywords: hydra
-;; URL: http://github.com/dustinlacewell/hera
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24") (helm "0") (dash "0") (org "0"))
+;; Keywords: bookmarks, org, helm
+;; URL: http://github.com/dustinlacewell/linkmarks
 
 ;;; Commentary:
 
 ;; This package lets you use org-mode links as bookmarks.
 
 ;;; Code:
-(require 'seq)
-(require 'helm)
 (require 'dash)
+(require 'helm)
+(require 'org)
+(require 'seq)
 
-(setq linkmarks-file (expand-file-name "~/org/bookmarks.org"))
+;;;###autoload
+(defgroup linkmarks nil
+  "Configuration options for linkmarks."
+  :group 'org)
 
-(defun linkmarks--setup ()
+;;;###autoload
+(defcustom linkmarks-file (expand-file-name "~/org/bookmarks.org")
+  "Define linkmarks file to store your bookmarks in."
+  :group 'linkmarks
+  :type 'string)
+
+(cl-defun linkmarks--setup ()
   (setq org-outline-path-complete-in-steps nil)
   (setq org-link-frame-setup '((elisp . find-file)
                                (vm . vm-visit-folder-other-frame)
@@ -32,18 +41,18 @@
   (setq org-confirm-elisp-link-function nil)
   (setq safe-local-variable-values '((org-confirm-elisp-link-function . nil))))
 
-(defun linkmarks--in-file ()
+(cl-defun linkmarks--in-file ()
   (linkmarks--setup)
   (with-temp-buffer
     (insert-file-contents linkmarks-file t)
     (org-mode)
-    (beginning-of-buffer)
+    (goto-char (point-min))
     (outline-show-all)
     (cl-loop
      for target in (org-refile-get-targets)
      for element = (progn
                      (goto-char (nth 3 target))
-                     (next-line)
+                     (forward-line)
                      (org-element-context))
      for type = (car element)
      for props = (cadr element)
@@ -53,7 +62,8 @@
      if (equal 'link type)
      collect (list (car target) content))))
 
-(defun linkmarks-select ()
+;;;###autoload
+(cl-defun linkmarks-select ()
   (interactive)
   (-let* ((targets (linkmarks--in-file))
           (choices (mapcar 'car targets))
@@ -61,6 +71,7 @@
           ((_ link) (-first (lambda (i) (equal (car i) choice)) targets)))
     (org-open-link-from-string link)))
 
+;;;###autoload
 (defun linkmarks-capture ()
   (interactive)
   (let ((org-capture-entry '("t" "Bookmark" entry (file "~/org/bookmarks.org")
@@ -69,4 +80,5 @@
     (org-capture)))
 
 (provide 'linkmarks)
+
 ;;; linkmarks.el ends here
