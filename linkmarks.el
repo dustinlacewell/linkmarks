1686703ef529fbb154b1cf692ad41979e8a9a28f
Frederick Morlock <frederickgeek8@gmail.com>
2025-04-29 10:13:44 -0400

    Completely remove Helm as a dependency
    Commit 9ca42f4 aimed to remove Helm as a dependency, but Helm still
    existed in the documentation headers for linkmarks.el and linkmarks.org.

--- linkmarks.el
+++ linkmarks.el
@@ -3,8 +3,8 @@
 
 ;; Author: Dustin Lacewell <dlacewell@gmail.com>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24") (helm "0") (dash "0") (org "0"))
-;; Keywords: bookmarks, org, helm
+;; Package-Requires: ((emacs "24") (dash "0") (org "0"))
+;; Keywords: bookmarks, org
 ;; URL: http://github.com/dustinlacewell/linkmarks
 
 ;;; Commentary:
